8afadedaa05e8cb507fedaa62bc38054fb617ae3
vishalavalani <vishal.avalani@gmail.com>
2019-06-06 17:02:55 +0530

    Work on undo and clear Work on saving image Work on save image after drawing Test on iOS and Android Work on android fixes

--- App.js
+++ App.js
@@ -1,24 +1,64 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import * as React from "react";
+import { View, Text, StyleSheet, ImageBackground, Button, CameraRoll, Image } from "react-native";
+import Constants from "expo-constants";
 import RNDraw from "rn-draw";
+import { takeSnapshotAsync } from "expo";
 
 export default class App extends React.Component {
+  state = {
+    cameraRollUri: null
+  };
+  _saveToCameraRollAsync = async () => {
+    let result = await takeSnapshotAsync(this._container, {
+      format: "png",
+      result: "file"
+    });
+
+    let saveResult = await CameraRoll.saveToCameraRoll(result, "photo");
+    this.setState({ cameraRollUri: saveResult });
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <RNDraw
-          containerStyle={{ backgroundColor: "rgba(0,0,0,0.01)" }}
-          rewind={undo => {
-            this._undo = undo;
-          }}
-          clear={clear => {
-            this._clear = clear;
-          }}
-          color={"#000000"}
-          strokeWidth={4}
-        />
-      </View>
+      <>
+        <View style={styles.container}>
+          <View
+            ref={view => {
+              this._container = view;
+            }}
+            style={{ backgroundColor: "black" }}
+          >
+            <ImageBackground
+              source={{ uri: "https://s-ec.bstatic.com/images/hotel/max1024x768/731/73118462.jpg" }}
+              style={{ width: "100%", height: "100%" }}
+            >
+              <RNDraw
+                containerStyle={{ backgroundColor: "rgba(0,0,0,0.01)" }}
+                rewind={undo => {
+                  this._undo = undo;
+                }}
+                clear={clear => {
+                  this._clear = clear;
+                }}
+                color={"red"}
+                strokeWidth={4}
+              />
+            </ImageBackground>
+          </View>
+          <View style={{ flexDirection: "row", justifyContent: "space-around", height: 80 }}>
+            <Button onPress={() => this._undo()} title="Undo" />
+            <Button onPress={() => this._clear()} title="Clear" />
+            <Button onPress={this._saveToCameraRollAsync} title="Capture" />
+          </View>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "yellow" }}>
+          {this.state.cameraRollUri && (
+            <Image source={{ uri: this.state.cameraRollUri }} style={{ width: "100%", height: "100%" }} />
+          )}
+          {!this.state.cameraRollUri && (
+            <Text style={{ fontSize: 20, padding: 20 }}>Captured image will be displayed here...</Text>
+          )}
+        </View>
+      </>
     );
   }
 }
@@ -26,9 +66,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    backgroundColor: "#ecf0f1",
+    padding: 8
   }
 });
