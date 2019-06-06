e43e1b1f355ea5473d57ea800cf039996f257df8
vishalavalani <vishal.avalani@gmail.com>
2019-06-06 15:12:24 +0530

    Try various fixes

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
 import { View, StyleSheet } from "react-native";
+import RNDraw from "rn-draw";
+import Constants from "expo-constants";
 
 export default class App extends Component {
   render() {
-    return <View style={styles.container} />;
+    return (
+      <View style={styles.container}>
+        <RNDraw
+          strokes={[]}
+          containerStyle={{ backgroundColor: "rgba(0,0,0,0.01)" }}
+          rewind={undo => {
+            this._undo = undo;
+          }}
+          clear={clear => {
+            this._clear = clear;
+          }}
+          color={"#000000"}
+          strokeWidth={4}
+          onChangeStrokes={strokes => console.log(strokes)}
+        />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellow"
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8
   }
 });
