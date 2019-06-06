5e8053bdbda95ae8e61c705140bdb87e63bdf310
vishalavalani <vishal.avalani@gmail.com>
2019-06-06 14:35:28 +0530

    Project cleanup

--- App.js
+++ App.js
@@ -1,24 +1,9 @@
 import React, { Component } from "react";
 import { View, StyleSheet } from "react-native";
-import RNDraw from "rn-draw";
 
 export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <RNDraw
-          containerStyle={{ backgroundColor: "rgba(0,0,0,0.01)" }}
-          rewind={undo => {
-            this._undo = undo;
-          }}
-          clear={clear => {
-            this._clear = clear;
-          }}
-          color={"#000000"}
-          strokeWidth={4}
-        />
-      </View>
-    );
+    return <View style={styles.container} />;
   }
 }
 
